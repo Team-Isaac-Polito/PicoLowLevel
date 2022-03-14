54c3629864ef253bdbe90c11209022d954cab4fd
Andrea Grillo <grilloandrea6@gmail.com>
2022-03-14 16:13:08 +0100

    small typos

--- PID.cpp
+++ PID.cpp
@@ -1,19 +1,15 @@
 #include "PID.h"
 
 PID::PID(float kp, float ki, float kd) {
-	 
-    KP = kp;
-  KI = ki;
-  KP = kd;
-
-  //ToDo inizializzare altre variabili, sumErr, [old]feeback
-    
+	KP = kp;
+	KI = ki;
+	KP = kd;
+	//ToDo inizializzare altre variabili, sumErr, [old]feeback
 }
 
 void PID::updateReferenceValue(float ref) {
 	referenceValue = ref;
-
-	// azzerare sumErr?
+	sumErr = 0;	
 }
 
 void PID::updateFeedback(float fb) {
@@ -23,7 +19,7 @@ void PID::updateFeedback(float fb) {
 
 float PID::calculate() {
 	float output;
-	//velocità di output Encoder Relativo
+
 	output = (referenceValue - feedback) * KP;   // contributo proporzionale
 	output += (sumErr * KI * DT / 1000);         // contributo integrativo
 	output +=  -(1000 * KD * (feedback - oldFeedback) / DT ); //contributo derivativo
