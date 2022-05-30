20375ae8552f3e3c419daaf24e48b233e3173a41
Andrea Grillo <grilloandrea6@gmail.com>
2022-05-30 21:58:48 +0200

    removed redundant references to ams_ class

--- AbsoluteEncoder.cpp
+++ AbsoluteEncoder.cpp
@@ -9,19 +9,19 @@ AbsoluteEncoder::AbsoluteEncoder() {
 
 void AbsoluteEncoder::begin() {
   AMS_AS5048B::begin();
-  AMS_AS5048B::setClockWise(true); 
+  setClockWise(true); 
 
   setZero();
  }
 
 void AbsoluteEncoder::setZero() {
-  AMS_AS5048B::zeroRegW(0x0);
+  zeroRegW(0x0);
 }
 
 float AbsoluteEncoder::readAngle() {
-  return AMS_AS5048B::angleR(U_DEG, false);
+  return angleR(U_DEG, false);
 }
 
 void AbsoluteEncoder::update() {
-  AMS_AS5048B::updateMovingAvgExp();
+  updateMovingAvgExp();
 }
