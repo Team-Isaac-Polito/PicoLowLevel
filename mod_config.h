7f81ef619cd851ab3c02100e6058fbba16aef284
Andrea Grillo <grilloandrea6@gmail.com>
2022-10-05 10:21:55 +0200

    servo addresses set

--- mod_config.h
+++ mod_config.h
@@ -13,8 +13,8 @@
 #define CAN_ID    0x15  // HEAD
 #define MODC_EE
 #define SERVO_PITCH_ID 1
-#define SERVO_ROLL_ID 45
-#define SERVO_PITCH2_ID 46
+#define SERVO_ROLL_ID 4
+#define SERVO_PITCH2_ID 2
 #define SERVO_MIN 200
 #define SERVO_MAX 800
 #define SERVO_SPEED 200
