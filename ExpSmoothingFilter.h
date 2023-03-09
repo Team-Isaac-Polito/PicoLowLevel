944b317c71c7df464552e5e67fe85bbcb4348eb5
Marco Fincato <marcofincato777@gmail.com>
2023-03-10 00:17:18 +0100

    Created exponential smoothing filter

---
+++ ExpSmoothingFilter.h
@@ -0,0 +1,33 @@
+#ifndef EXP_SMOOTHING_FILTER_H
+#define EXP_SMOOTHING_FILTER_H
+
+#include "Filter.h"
+
+/**
+ * Class for a exponential smoothing filter implementing the Filter interface.
+ * @tparam T type of the variable to be filtered.
+ */
+template<typename T>
+class ExpSmoothingFilter: public Filter<T> {
+  public:
+    ExpSmoothingFilter(int a, int b) {
+        this->a = a;
+        this->b = b;
+        previous = 0;
+    }
+
+    /**
+     * Computes the filtered output using an exponential filter, giving the passed value a weight of a/b.
+     * @param value to be filtered. 
+     * @return T filtered output.
+     */
+    T filter(T value) {
+        return previous = (value*a + previous*(b-a))/b;
+    }
+    
+  private:
+    T previous;
+    int a, b;
+};
+
+#endif
